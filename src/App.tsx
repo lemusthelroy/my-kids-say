bf6e21da60696286f574c4631ad75a636d49bc7d
Lewis Thorley <lewis.thorley@netlify.com>
2022-11-02 12:53:52 +0000

    Env fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,38 @@ const client = createClient({
   accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN as string,
 });
 
-const App = () => {
-  console.log(process.env.CONTENTFUL_SPACE);
+const shuffle = (array: Entry<IKidPost>[]) => {
+  let currentIndex = array.length,
+    randomIndex;
 
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+const App = () => {
   const [entries, setEntries] = useState<Entry<IKidPost>[]>();
 
-  const [currentEntry, setCurrentEntry] = useState<Entry<IKidPost>>();
+  const [currentEntry, setCurrentEntry] = useState<number>(0);
 
   const getNextQuote = () => {
     if (entries) {
-      const randomElement = entries[Math.floor(Math.random() * entries.length)];
-      setCurrentEntry(randomElement);
+      if (currentEntry + 1 >= entries.length) {
+        setCurrentEntry(0);
+      } else {
+        setCurrentEntry(currentEntry + 1);
+      }
     }
   };
 
@@ -39,8 +60,7 @@ const App = () => {
     client
       .getEntries<IKidPost>()
       .then((response) => {
-        setEntries(response.items);
-        getNextQuote();
+        setEntries(shuffle(response.items));
       })
       .catch(console.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -49,7 +69,11 @@ const App = () => {
   return (
     <div className="flex h-screen items-center align-middle w-full flex-col justify-center self-center">
       <div className="border-t-[1px] border-b-[1px] p-4 border-gray-400 mb-4">
-        <ReactMarkdown>{currentEntry?.fields.body ?? ""}</ReactMarkdown>
+        {entries && (
+          <ReactMarkdown>
+            {entries[currentEntry].fields.body ?? ""}
+          </ReactMarkdown>
+        )}
       </div>
 
       <button
