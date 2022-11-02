790e57dcea011e274510da8f621d6f5c4b850316
Lewis Thorley <lewis.thorley@netlify.com>
2022-11-02 12:27:30 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Entry, createClient } from "contentful";
+import ReactMarkdown from "react-markdown";
 
-function App() {
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE as string,
+  environment: "master", // defaults to 'master' if not set
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+});
+
+interface IKidPost {
+  title: string;
+  body: string;
+}
+
+const App = () => {
+  const [entries, setEntries] = useState<Entry<IKidPost>[]>();
+
+  const [currentEntry, setCurrentEntry] = useState<Entry<IKidPost>>();
+
+  const getNextQuote = () => {
+    if (entries) {
+      const randomElement = entries[Math.floor(Math.random() * entries.length)];
+      setCurrentEntry(randomElement);
+    }
+  };
+
+  useEffect(() => {
+    if (entries) {
+      getNextQuote();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [entries]);
+
+  useEffect(() => {
+    client
+      .getEntries<IKidPost>()
+      .then((response) => {
+        setEntries(response.items);
+        getNextQuote();
+      })
+      .catch(console.error);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen items-center align-middle w-full flex-col justify-center self-center">
+      <div className="border-t-[1px] border-b-[1px] p-4 border-gray-400 mb-4">
+        <ReactMarkdown>{currentEntry?.fields.body ?? ""}</ReactMarkdown>
+      </div>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={getNextQuote}
+      >
+        Next
+      </button>
     </div>
   );
-}
+};
 
 export default App;
